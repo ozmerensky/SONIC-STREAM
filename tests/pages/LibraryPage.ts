619610557ad8fff7df9ec580fe7afbc4aeb83b07
Oz Merensky <oz.meresnky@gmail.com>
2026-07-09 09:02:51 +0300

    chore: remove unused goto method from LibraryPage

--- tests/pages/LibraryPage.ts
+++ tests/pages/LibraryPage.ts
@@ -10,10 +10,4 @@ export class LibraryPage {
     this.trackCards = page.getByTestId('track-card');
     this.emptyMessage = page.getByText(/songs you like will appear here/i);
   }
-
-  async goto() {
-    await this.page.goto('/library');
-    await this.page.waitForURL('**/library');
-  }
 }
-
